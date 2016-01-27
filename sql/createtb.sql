204c7c122b95e6e88986272bc03593080e2e4f27
xiaowing <wing0630@gmail.com>
2016-01-27 23:03:18 +0800

    update the ddl script to replace the user.

--- sql/createtb.sql
+++ sql/createtb.sql
@@ -5,19 +5,22 @@
 DROP TABLE IF EXISTS m_sch.m_user_auth;
 DROP SCHEMA IF EXISTS m_sch;
 
-CREATE SCHEMA m_sch AUTHORIZATION postgres;
-CREATE TABLE m_sch.m_user_auth
-(
+DROP ROLE IF EXISTS tinysso;
+CREATE USER tinysso NOSUPERUSER NOCREATEDB NOCREATEROLE ENCRYPTED PASSWORD 'asdf1234';
+
+CREATE SCHEMA m_sch AUTHORIZATION tinysso;
+CREATE TABLE m_sch.m_user_auth(
+    user_serial serial NOT NULL,
     user_id character varying(20) NOT NULL,
     user_password character varying(20) NOT NULL,
     user_mail text NOT NULL,
+    signup_time  timestamp(3) DEFAULT now(),
     user_actived boolean NOT NULL DEFAULT false,
-    CONSTRAINT m_user_auth_pkey PRIMARY KEY (user_id),
-    CONSTRAINT m_user_auth_ukey UNIQUE (user_mail)
-)
-WITH (
-    OIDS=FALSE
+    PRIMARY KEY (user_serial),
+    CONSTRAINT m_user_auth_ukey UNIQUE (user_id),
+    CONSTRAINT m_user_auth_mail_ukey UNIQUE (user_mail)
 );
-ALTER TABLE m_sch.m_user_auth OWNER TO postgres;
+ALTER TABLE m_sch.m_user_auth OWNER TO tinysso;
 
-INSERT INTO m_sch.m_user_auth VALUES ('xiaowing', 'asdf1234', 'xiaowing@xyz.com');
+INSERT INTO m_sch.m_user_auth (user_id, user_password, user_mail, user_actived) VALUES ('xiaowing', 'asdf1234', 'xiaowing@xyz.com', TRUE);
+
